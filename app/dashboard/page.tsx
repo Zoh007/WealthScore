ff0eaeeb653d978cf4558d37f3e5188b4487065b
Nathan <nathandong68@gmail.com>
2025-09-27 13:17:23 -0400

    score breakdown

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,7 @@ import {
   CardTitle,
   CardContent,
 } from "@/components/ui/card"
+import { ScoreBreakdownAccordion } from "@/components/ScoreBreakdownAccordian";
 
 let score = 67
 let trend = 3
@@ -19,7 +20,6 @@ let actionItem1 = "You've saved an extra $150 this month. Great job!"
 let actionItem2 = "'Dining Out' spending is 15\% higher than last month."
 let actionItem3 = "Next bill: Internet ($60) is due in 3 days."
 
-
 export default function Dashboard() {
   return (
     <Container className="flex flex-wrap flex-col w-full p-12">
@@ -27,7 +27,7 @@ export default function Dashboard() {
         <h1 className="text-5xl font-semibold text-indigo-600">Welcome back, <span className="text-gray-800">{name}</span></h1>
         <p className="mt-2 text-gray-600">Here's your financial wellness at a glance.</p>
       </header>
-      <main className="flex items-center justify-around w-full">
+      <div className="flex items-center justify-around w-full">
         <Card className="w-1/5">
           <CardTitle>Cash Flow</CardTitle>
           <CardContent className="text-gray-600">So far this month, you have earned:</CardContent>
@@ -44,7 +44,10 @@ export default function Dashboard() {
           <CardContent className="bg-yellow-50 text-yellow-700 p-3 rounded-lg text-sm mx-3">{actionItem2}</CardContent>
           <CardContent className="bg-blue-50 text-blue-700 p-3 rounded-lg text-sm mx-3">{actionItem3}</CardContent>
         </Card>
-      </main>
+      </div>
+      <div className="mt-12 w-full px-6 mx-auto">
+        <ScoreBreakdownAccordion />
+      </div>
     </Container>
   );
 }
