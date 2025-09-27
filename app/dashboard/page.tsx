df2122ee7d8ba4524161be28ec757b58d8baaa2b
Nathan <nathandong68@gmail.com>
2025-09-27 05:27:35 -0400

    added additional info cards next to main score

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { FinancialScoreDisplay } from "@/components/FinancialScoreDisplay";
 import {
   Card,
+  CardTitle,
   CardContent,
 } from "@/components/ui/card"
 
@@ -11,19 +12,37 @@ let score = 67
 let trend = 3
 let status = "Good"
 let name = "Nathan"
+let earned = 2345
+let spent = 1331
+let saved = earned - spent > 0 ? earned - spent : 0
+let actionItem1 = "You've saved an extra $150 this month. Great job!"
+let actionItem2 = "'Dining Out' spending is 15\% higher than last month."
+let actionItem3 = "Next bill: Internet ($60) is due in 3 days."
+
 
 export default function Dashboard() {
   return (
     <Container className="flex flex-wrap min-h-screen w-full p-12">
-      <header className="mb-8">
+      <header>
         <h1 className="text-5xl font-semibold text-gray-800">Good morning, {name}</h1>
         <p className="mt-2 text-gray-500">Here's your financial wellness at a glance.</p>
       </header>
       <main className="flex items-center justify-around w-full">
         <Card className="w-1/5">
+          <CardTitle>Cash Flow</CardTitle>
+          <CardContent className="text-gray-600">So far this month, you have earned:</CardContent>
+          <CardContent className="flex text-2xl font-bold text-green-600 justify-center">${earned}</CardContent>
+          <CardContent className="text-gray-600">And have spent:</CardContent>
+          <CardContent className="flex text-2xl font-bold text-red-400 justify-center">${spent}</CardContent>
+          <CardContent className="text-gray-600">You are on track to save:</CardContent>
+          <CardContent className="flex text-2xl font-bold text-blue-500 justify-center">${saved}</CardContent>
         </Card>
-        <FinancialScoreDisplay score={67} trend={3} status="Good" />
+        <FinancialScoreDisplay score={score} trend={trend} status={status} />
         <Card className="w-1/5">
+          <CardTitle>Action Items</CardTitle>
+          <CardContent className="bg-green-50 text-green-700 p-3 rounded-lg text-sm mx-3">{actionItem1}</CardContent>
+          <CardContent className="bg-yellow-50 text-yellow-700 p-3 rounded-lg text-sm mx-3">{actionItem2}</CardContent>
+          <CardContent className="bg-blue-50 text-blue-700 p-3 rounded-lg text-sm mx-3">{actionItem3}</CardContent>
         </Card>
       </main>
     </Container>
