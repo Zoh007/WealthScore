8525a44f0eac7ee837a746317f9d382ced073674
Ifaz Howlader <ifaz.howl@gmail.com>
2025-09-28 05:14:09 -0400

    removing extra calendar stuff

--- app/calendar/layout.tsx
+++ app/calendar/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Settings } from "lucide-react";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
@@ -7,10 +6,6 @@ import { AppSidebar } from "@/components/app-sidebar";
 import "./../globals.css";
 
 import { CalendarProvider } from "@/calendar/contexts/calendar-context";
-import { ChangeBadgeVariantInput } from "@/calendar/components/change-badge-variant-input";
-import { ChangeVisibleHoursInput } from "@/calendar/components/change-visible-hours-input";
-import { ChangeWorkingHoursInput } from "@/calendar/components/change-working-hours-input";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 import { getEvents, getUsers } from "@/calendar/requests";
 
@@ -29,25 +24,6 @@ export default async function CalendarLayout({ children }: Readonly<{ children: 
         <CalendarProvider users={users} events={events}>
           <div className="mx-auto flex max-w-screen-2xl flex-col gap-4 px-8 py-4">
             {children}
-
-            <Accordion type="single" collapsible>
-              <AccordionItem value="item-1" className="border-none">
-                <AccordionTrigger className="flex-none gap-2 py-0 hover:no-underline">
-                  <div className="flex items-center gap-2">
-                    <Settings className="size-4" />
-                    <p className="text-base font-semibold">Calendar settings</p>
-                  </div>
-                </AccordionTrigger>
-
-                <AccordionContent>
-                  <div className="mt-4 flex flex-col gap-6">
-                    <ChangeBadgeVariantInput />
-                    <ChangeVisibleHoursInput />
-                    <ChangeWorkingHoursInput />
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
           </div>
         </CalendarProvider>
       </div>
